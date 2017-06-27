c269850ee9a2783d9977c39a4985a33199b40419
HKeating <horacekeating@hotmail.co.uk>
2017-06-27 16:31:34 +0100

    tests passing except last one

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -1,6 +1,15 @@
 var Words = function() {
   this.count = function(str) {
-    return str.split(' ').length;
+    var wordsArray = str.toLowerCase().replace(/[-`~¡!@#$%^&*()_|+=¿?;:",.<>\{\}\[\]\\\/\s]/gi, ' ').split(' ');
+    var wordsObj = {};
+    wordsArray.forEach(word => {
+      word = word.replace(/^\'|\'$/g, '');
+      console.log(word);
+      word === '' ? null : !wordsObj[word] ? wordsObj[word] = 1 : wordsObj[word] ++;
+    });
+    
+
+    return wordsObj;
   };
 };
 

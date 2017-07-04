794d2261e85cce51e1a8cbd70fe70ea9c30d6f85
HKeating <horacekeating@hotmail.co.uk>
2017-07-04 11:26:54 +0100

    fixed word-count with chris ziegler's tip

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -1,15 +1,12 @@
 var Words = function() {
   this.count = function(str) {
     var wordsArray = str.toLowerCase().replace(/[-`~¡!@#$%^&*()_|+=¿?;:",.<>\{\}\[\]\\\/\s]/gi, ' ').split(' ');
-    var wordsObj = {};
+    var wordsObj = Object.create(null);
     wordsArray.forEach(word => {
       word = word.replace(/^\'|\'$/g, '');
 
       word === '' ? null : !wordsObj[word] ? wordsObj[word] = 1 : wordsObj[word] ++;
     });
-    // console.log('Prototype stuff: ', Object.keys(wordsObj));
-    // console.log(str, ' : ', wordsObj);
-    // wordsObj[constructor] ? wordsObj[constructor] = 1 : null;
     return wordsObj;
   };
 };

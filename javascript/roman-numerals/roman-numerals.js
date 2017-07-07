f76a53912b6e227f7090edd895e03074d5f98207
HKeating <horacekeating@hotmail.co.uk>
2017-07-07 16:52:54 +0100

    more stuff

--- javascript/roman-numerals/roman-numerals.js
+++ javascript/roman-numerals/roman-numerals.js
@@ -1,5 +1,9 @@
 var toRoman = function(num) {
+  let workingNum = num;
   let result = '';
+  const M = Math.floor(num/1000);
+  workingNum -= M*1000;
+
   console.log('M', Math.floor(num/1000));
   console.log('D', Math.floor(num/500));
   console.log('C', Math.floor(num/100));
@@ -8,6 +12,21 @@ var toRoman = function(num) {
   console.log('V', Math.floor(num/5));
   console.log('I', Math.floor(num/1));
 
+};
+
+function numeralise(num, numeralValue) {
+  const letter = Objects.keys(numerals).find(numeralValue);
+  
+};
+
+const numerals = {
+  'M': 1000,
+  'D': 500,
+  'C': 100,
+  'L': 50,
+  'X': 10,
+  'V': 5,
+  'I': 1
 };
 
 module.exports = toRoman;

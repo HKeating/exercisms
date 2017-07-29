dbe45e0fff3b0474d54470e6c71a1d698f914e5a
HKeating <horacekeating@hotmail.co.uk>
2017-07-29 13:51:36 +0100

    circular buffer passing all tests

--- javascript/circular-buffer/circular-buffer.js
+++ javascript/circular-buffer/circular-buffer.js
@@ -1,20 +1,54 @@
 var circularBuffer = function(a) {
-  this.length = a;
+  return new Buffer(a);
+};
+
+var Buffer = function(b) {
+  this.length = b;
   this.values = [];
-  return a;
+
+  this.read = function() {
+    if (this.values) {
+      const readValue = this.values[0];
+      this.values.shift();
+      return readValue;
+    } else {
+      throw bufferEmptyException();
+    }
+  };
+
+  this.write = (x) => {
+    if (this.length === this.values.length) {
+      throw bufferFullException();
+    } else {
+      if(x) {
+        this.values.push(x);
+      }
+    }
+  };
+
+  this.forceWrite = (x) => {
+    if(x) {
+      if (this.length === this.values.length) {
+        this.values.shift();
+      }
+      this.values.push(x);
+    }
+  };
+
+  this.clear = () => {
+    this.values = [];
+    this.currentPos = 0;
+  };
 };
 
-circularBuffer.prototype.write = function(x) {
-  this.values.push(x);
-  return x;
-};
 
-circularBuffer.prototype.read = function() {
-  return this.values || bufferEmptyException;
-};
 
-var bufferEmptyException = new Error();
-var bufferFullException = new Error();
+const bufferEmptyException = () => {
+  return new Error();
+};
+const bufferFullException = () => {
+  return new Error();
+};
 
 
 module.exports = {
@@ -22,8 +56,3 @@ module.exports = {
   bufferEmptyException,
   bufferFullException
 };
-
-
-var buffer = circularBuffer(2);
-
-console.log('BUFFER: ', buffer);
